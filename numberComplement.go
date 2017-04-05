4e34c812e14b84052a77a2b642354d44bb130c8a
Rex Niu <jniu@LETV-1180.local>
2017-04-05 01:31:23 -0700

    better solution for number complement

--- numberComplement.go
+++ numberComplement.go
@@ -2,11 +2,7 @@ package Leetcode
 
 //Also able to handle negative input
 func findComplement(num int) int {
-	unum := uint32(num)
-	var xorInt uint32 = 1
-	var shiftNum uint32 = unum >> 1
-	for ; shiftNum != 0; shiftNum >>= 1 {
-		xorInt = xorInt << 1 + 1
-	}
-	return int(unum ^ xorInt)
+	var mask int = ^0
+	for ; mask & num != 0; mask <<= 1 {}
+	return num ^ (^mask)
 }
