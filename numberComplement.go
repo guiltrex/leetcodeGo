bdb28d8d045e77e6546e79478a84b1b866c0e288
Rex Niu <jniu@LETV-1180.local>
2017-04-05 00:09:53 -0700

    add solution to number complement

---
+++ numberComplement.go
@@ -0,0 +1,11 @@
+package Leetcode
+
+func findComplement(num int) int {
+	unum := uint32(num)
+	var xorInt uint32 = 1
+	var shiftNum uint32 = unum >> 1
+	for ; shiftNum != 0; shiftNum >>= 1 {
+		xorInt = xorInt << 1 + 1
+	}
+	return int(unum ^ xorInt)
+}
